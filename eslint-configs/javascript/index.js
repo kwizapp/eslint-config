c1dcfbb2cf1ef3cffa7389135077f82cddfa81f3
Nik Zaugg <zauggnik@gmail.com>
2020-03-24 09:26:27 +0100

    Update parserOptions in eslint base config (#9)
    * add parserOptions ecmaVersion 2020
    
    * @kwizapp/eslint-config-js v0.0.4
    
    * release v0.0.7
    
    * release v0.0.8

--- eslint-configs/javascript/index.js
+++ eslint-configs/javascript/index.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:prettier/recommended"]
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  // define parser options to make features such as async/wait work
+  parserOptions: {
+    ecmaVersion: 2020 // same as ECMAScript 10
+  }
 };
