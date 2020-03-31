3e60cae2fea0074980bb97ef8b04e13c5cdc30e6
Roland Schläfli <rolandschlaefli@gmail.com>
2020-03-31 14:08:25 +0200

    Fix: Disable member-delimiter-style as it conflicts with prettier

--- eslint-configs/typescript/index.js
+++ eslint-configs/typescript/index.js
@@ -9,6 +9,7 @@ module.exports = {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "error"
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/member-delimiter-style": "off"
   }
 };
